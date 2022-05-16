1664679f40f393fda81c42070f7691ec27c0a680
Sam Smith <samlsmith424@gmail.com>
2022-05-16 16:31:49 -0700

    Test: add warehouse relationship to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  belongs_to :warehouse
   validates_presence_of :name, :description, :price
   validates_numericality_of :price
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
+  describe 'relationships' do
+    it { should belong_to(:warehouse) }
+  end
+
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:description) }
