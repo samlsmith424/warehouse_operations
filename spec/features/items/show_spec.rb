e6110c8387ffa95322666154bcb7940526e49fcb
Sam Smith <samlsmith424@gmail.com>
2022-05-16 09:23:31 -0700

    Test: item show page contains item attributes

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -3,14 +3,17 @@ require 'rails_helper'
 RSpec.describe 'Item Show Page' do
   it 'can display the item attributes' do
     item = FactoryBot.create(:item)
+
     visit item_path(item)
 
-    expect(page).to have_content("Inventory Items")
-    expect(items.count).to eq(3)
-    # items.each do |item|
-    #   expect(item.name).to be_a(String)
-    #   expect(item.description).to be_a(String)
-    #   expect(item.price).to be_a(Float)
-    # end
+    expect(page).to have_content("Item Information")
+    expect(page).to have_content(item.name)
+    expect(item.name).to be_a(String)
+
+    expect(page).to have_content(item.description)
+    expect(item.description).to be_a(String)
+
+    expect(page).to have_content(item.price)
+    expect(item.price).to be_a(Float)
   end
 end
