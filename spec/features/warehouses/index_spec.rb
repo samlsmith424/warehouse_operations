1e9234d362a57e8eb6be959fe6f7769753919915
Sam Smith <samlsmith424@gmail.com>
2022-05-16 17:27:07 -0700

    Test: warehouse index page

--- spec/features/warehouses/index_spec.rb
+++ spec/features/warehouses/index_spec.rb
@@ -7,12 +7,12 @@ RSpec.describe "Warehouses Index Page" do
     expect(page).to have_content("Warehouses")
   end
 
-  xit 'has a button to create a warehouse' do
+  it 'has a button to create a warehouse' do
     visit warehouses_path
     expect(page).to have_button("Create Warehouse")
 
     click_button("Create Warehouse")
 
-    expect(current_path).to eq()
+    expect(current_path).to eq(new_warehouse_path)
   end
 end
